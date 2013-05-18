66c18a4ddc981c82427db7dc289635db639073cc
onema <kinojman@gmail.com>
2013-05-17 23:30:43 -0700

    updated old names

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('rest_api_crud');
+        $rootNode = $treeBuilder->root('base_api');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
